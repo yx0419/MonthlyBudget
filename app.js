dcf509b2ede25cc8b5a7d4fa404c126f935a7aaf
Yunjae Cho <yunxics@gmail.com>
2020-03-12 16:41:34 -0700

    fixed some minor error

--- app.js
+++ app.js
@@ -267,6 +267,7 @@ var UIController = (function () {
 
             now = new Date();
 
+
             months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
             month = now.getMonth();
 
